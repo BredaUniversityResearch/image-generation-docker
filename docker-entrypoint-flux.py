614b4ace042bdaf3454c656c5d0d57c8b871f020
suryashree <30572296+suryashree@users.noreply.github.com>
2024-10-02 16:08:24 +0200

    Create docker entrypoint using flux + controlnet (#4)
    * Add flux implementation
    
    * Controlnet for flux

--- docker-entrypoint-flux.py
+++ docker-entrypoint-flux.py
@@ -11,27 +11,12 @@ from typing import Optional
 import numpy as np
 import torch
 import uvicorn
-from diffusers import (
-    AutoPipelineForImage2Image,
-    AutoPipelineForInpainting,
-    AutoPipelineForText2Image,
-    ControlNetModel,
-    DiffusionPipeline,
-    OnnxStableDiffusionImg2ImgPipeline,
-    OnnxStableDiffusionInpaintPipeline,
-    OnnxStableDiffusionPipeline,
-    StableDiffusionXLControlNetPipeline,
-    schedulers,
-)
+from diffusers import FluxPipeline, FluxControlNetModel, FluxControlNetPipeline
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from PIL import Image
 from pydantic import BaseModel, ConfigDict, Field
 from rembg import remove
-
-# empty gpu cache
-torch.cuda.empty_cache()
-gc.collect
 
 app = FastAPI()
 
@@ -52,13 +37,10 @@ class ImageGenerationConfig(BaseModel):
         description="Use for generating character with no background and specific poses",
     )
     controlnet_conditioning_scale: float = Field(
-        1.0, description="Generalization for controlnet"
+        0.6, description="Generalization for controlnet"
     )
     device: str = Field(
         "cuda", description="The cpu or cuda device to use to render images"
-    )
-    half: bool = Field(
-        False, description="Use float16 (half-sized) tensors instead of float32"
     )
     height: int = Field(512, description="Image height in pixels")
     image: Optional[str] = Field(
@@ -71,33 +53,23 @@ class ImageGenerationConfig(BaseModel):
         None, description="The image used as base for ip adapter"
     )
     iters: int = Field(1, description="Number of times to run pipeline")
-    mask: Optional[str] = Field(
-        None, description="The input mask to use for diffusion inpainting"
+    max_sequence_length: int = Field(
+        256, description="Maximum sequence length. Cannot be over 256 for flux schnell."
     )
     model: str = Field(
-        "stabilityai/stable-diffusion-xl-base-1.0",
+        "black-forest-labs/FLUX.1-schnell",
         description="The model used to render images",
     )
-    negative_prompt: Optional[str] = Field(
-        None, description="The prompt to not render into an image"
-    )
-    onnx: bool = Field(False, description="Use the onnx runtime for inference")
     prompt: Optional[str] = Field(
         None, description="The prompt to render into an image"
     )
     samples: int = Field(1, description="Number of images to create per run")
     scale: float = Field(
-        30, description="How closely the image should follow the prompt"
+        0, description="How closely the image should follow the prompt"
     )
-    scheduler: Optional[str] = Field(
-        None, description="Override the scheduler used to denoise the image"
-    )
+
     seed: int = Field(42, description="RNG seed for repeatability")
-    skip: bool = Field(False, description="Skip the safety checker")
-    steps: int = Field(60, description="Number of sampling steps")
-    strength: float = Field(
-        0.75, description="Diffusion strength to apply to the input image"
-    )
+    steps: int = Field(4, description="Number of sampling steps")
     token: Optional[str] = Field(None, description="Huggingface user access token")
     vae_slicing: bool = Field(
         False, description="Use less memory when creating large batches of images"
@@ -106,9 +78,6 @@ class ImageGenerationConfig(BaseModel):
         False, description="Use less memory when creating ultra-high resolution images"
     )
     width: int = Field(512, description="Image width in pixels")
-    xformers_memory_efficient_attention: bool = Field(
-        False, description="Use less memory but require the xformers library"
-    )
     dtype: Optional[torch.dtype] = None
     controlnet: Optional[torch.nn.Module] = None
     diffuser: Optional[torch.nn.Module] = None
@@ -131,19 +100,19 @@ def remove_unused_args(p):
     params = inspect.signature(p.pipeline).parameters.keys()
     args = {
         "prompt": p.prompt,
-        "negative_prompt": p.negative_prompt,
-        "image": p.image,
-        "ip_adapter_image": p.ip_adapter_image,
-        "controlnet_conditioning_scale": p.controlnet_conditioning_scale,
-        "mask_image": p.mask,
         "height": p.height,
         "width": p.width,
         "num_images_per_prompt": p.samples,
         "num_inference_steps": p.steps,
         "guidance_scale": p.scale,
+        "controlnet": p.controlnet,
+        "control_image": p.image,
+        "control_mode": 4,
         "image_guidance_scale": p.image_scale,
-        "strength": p.strength,
+        "controlnet_conditioning_scale": p.controlnet_conditioning_scale,
+        "ip_adapter_image": p.ip_adapter_image,
         "generator": p.generator,
+        "max_sequence_length": p.max_sequence_length,
     }
     return {p: args[p] for p in params if p in args}
 
@@ -167,56 +136,18 @@ def remove_background(image_location: str):
     return
 
 
-def stable_diffusion_pipeline(p):
-    p.dtype = torch.float16 if p.half else torch.float32
+def flux_pipeline(p):
+    p.dtype = torch.float16
 
-    if p.onnx:
-        p.diffuser = OnnxStableDiffusionPipeline
-        p.revision = "onnx"
-    else:
-        p.diffuser = DiffusionPipeline
-        p.revision = "fp16" if p.half else "main"
-
-    autos = argparse.Namespace(
-        **{
-            "sd": ["StableDiffusionPipeline"],
-            "sdxl": ["StableDiffusionXLPipeline"],
-        }
-    )
-
-    config = DiffusionPipeline.load_config(p.model)
-    is_auto_pipeline = config["_class_name"] in [autos.sd, autos.sdxl]
-
-    if is_auto_pipeline:
-        p.diffuser = AutoPipelineForText2Image
+    p.diffuser = FluxPipeline
 
     if p.image is not None:
-        if p.revision == "onnx":
-            p.diffuser = OnnxStableDiffusionImg2ImgPipeline
-        elif is_auto_pipeline:
-            p.diffuser = AutoPipelineForImage2Image
         p.image = load_image(p.image)
-        p.ip_adapter_image = load_image(p.ip_adapter_image)
-
-    if p.mask is not None:
-        if p.revision == "onnx":
-            p.diffuser = OnnxStableDiffusionInpaintPipeline
-        elif is_auto_pipeline:
-            p.diffuser = AutoPipelineForInpainting
-        p.mask = load_image(p.mask)
-
-    if p.token is None:
-        with open("token.txt") as f:
-            p.token = f.read().replace("\n", "")
 
     if p.seed == 0:
         p.seed = torch.random.seed()
 
-    if p.revision == "onnx":
-        p.seed = p.seed >> 32 if p.seed > 2**32 - 1 else p.seed
-        p.generator = np.random.RandomState(p.seed)
-    else:
-        p.generator = torch.Generator(device=p.device).manual_seed(p.seed)
+    p.generator = torch.Generator(device=p.device).manual_seed(p.seed)
 
     print("load pipeline start:", iso_date_time(), flush=True)
 
@@ -230,45 +161,20 @@ def stable_diffusion_pipeline(p):
 
         if p.character == True:
             if p.image is not None:
-                p.controlnet = ControlNetModel.from_pretrained(
-                    "xinsir/controlnet-openpose-sdxl-1.0",
+                p.controlnet = FluxControlNetModel.from_pretrained(
+                    "InstantX/FLUX.1-dev-Controlnet-Union",
+                    torch_dtype=p.dtype,
                 )
 
-                p.diffuser = StableDiffusionXLControlNetPipeline
+                p.diffuser = FluxControlNetPipeline
 
         pipeline = p.diffuser.from_pretrained(
             p.model,
             controlnet=p.controlnet,
             torch_dtype=p.dtype,
-            revision=p.revision,
-            use_auth_token=p.token,
-        ).to(f"{p.device}:{device_id}")
-
-    if p.ip_adapter_image is not None:
-        pipeline.load_ip_adapter(
-            "h94/IP-Adapter", subfolder="sdxl_models", weight_name="ip-adapter_sdxl.bin"
         )
-        pipeline.ip_adapter_image = p.ip_adapter_image
-        pipeline.set_ip_adapter_scale(1.0)
 
-    if p.scheduler is not None:
-        scheduler = getattr(schedulers, p.scheduler)
-        pipeline.scheduler = scheduler.from_config(pipeline.scheduler.config)
-
-    if p.skip:
-        pipeline.safety_checker = None
-
-    if p.attention_slicing:
-        pipeline.enable_attention_slicing()
-
-    if p.xformers_memory_efficient_attention:
-        pipeline.enable_xformers_memory_efficient_attention()
-
-    if p.vae_slicing:
-        pipeline.enable_vae_slicing()
-
-    if p.vae_tiling:
-        pipeline.enable_vae_tiling()
+        pipeline.enable_sequential_cpu_offload()
 
     p.pipeline = pipeline
 
@@ -277,13 +183,14 @@ def stable_diffusion_pipeline(p):
     return p
 
 
-def stable_diffusion_inference(p):
+def flux_inference(p):
     prefix = (
         re.sub(r"[\\/:*?\"<>|]", "", p.prompt)
         .replace(" ", "_")
         .encode("utf-8")[:170]
         .decode("utf-8", "ignore")
     )
+
     for j in range(p.iters):
         result = p.pipeline(**remove_unused_args(p))
 
@@ -302,14 +209,17 @@ def stable_diffusion_inference(p):
 
 @app.post("/")
 def image_generation(p: ImageGenerationConfig):
-    pipeline = stable_diffusion_pipeline(p)
-    img = stable_diffusion_inference(pipeline)
+    pipeline = flux_pipeline(p)
+    img = flux_inference(pipeline)
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+        torch.cuda.ipc_collect()
     return img
 
 
 if __name__ == "__main__":
     uvicorn.run(
-        "docker-entrypoint:app",
+        "docker-entrypoint-flux:app",
         host="0.0.0.0",
         workers=4,
         port=3750,
